f8b38680500c2e1847916ae10d00265d670a7657
SrihariGitHub99 <142526038+SrihariGitHub99@users.noreply.github.com>
2023-09-09 12:58:10 +0530

    Update abc.groovy

--- vars/abc.groovy
+++ vars/abc.groovy
@@ -1,9 +1,4 @@
 def gitDownload(repo)
-    {
-	 git "https://github.com/SrihariGitHub99/${repo}.git"
-    }
-def newBuild
-   {
-	sh 'mvn package'
-   }
-
+{
+	git "https://github.com/SrihariGitHub99/${repo}.git"
+}
